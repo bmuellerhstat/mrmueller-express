88b4791fbfb139b3c113087d824a19b6a86f1af7
Mr. Mueller <bmueller@hstat.org>
2025-05-12 08:31:52 -0400

    hello world

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+  
+const app = express();
+const PORT = 5001;
+  
+app.get('/', (req, res)=>{
+    res.status(200);
+    res.send("Hello World");
+});
+  
+app.listen(PORT, (error) =>{
+    if(!error)
+        console.log(`Server is running at http://localhost:${PORT}`);
+    else 
+        console.log("Error occurred, server can't start", error);
+    }
+);
